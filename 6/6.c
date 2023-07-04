baca623e1e7b326a1057f7a2cc1f93495fa0d241
DinhPhuc24 <mrphuc244@gmail.com>
2023-07-04 16:11:32 +0700

    add 6

---
+++ 6/6.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main() {
+    int i, n;
+    float sum;
+    scanf("%d", &n);
+    sum = 0;
+    i = 1;
+    while (i<= 2* n) {
+        sum += 1.0/(i*(i + 1));
+        i++;
+    }
+    printf("%.2f", sum);
+}
