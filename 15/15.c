35ae0321ddaecc046700694836304640bc273247
DinhPhuc24 <mrphuc244@gmail.com>
2023-07-08 14:21:48 +0700

    add 15

---
+++ 15/15.c
@@ -0,0 +1,30 @@
+/*
+    By Kichirou24
+*/
+#include <stdio.h>
+
+int tinhtong(int x){
+    int z, r;
+    r = 0;
+    z = 1;
+    while (z <= x) {
+        r+= z;
+        z++;
+    }
+    return r;
+} 
+
+int main() {
+    int n, x, i, j, temp; 
+    double sum;
+    scanf("%d", &n );
+    sum = 1;
+    temp = 1;
+    i = 2;
+    while (i <= n) {
+        temp = tinhtong(i);
+        sum+= 1.0/temp;
+        i++;
+    }
+    printf("%.2f", sum);
+}
